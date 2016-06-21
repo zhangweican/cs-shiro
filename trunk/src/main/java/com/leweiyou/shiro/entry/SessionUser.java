5563df7b4597ccdd87ef854b1ce680ab6b3f4815
zhangweican <515632400@qq.com>
2016-06-21 10:18:02 +0800

    扩展session缓存用户信息

--- trunk/src/main/java/com/leweiyou/shiro/entry/SessionUser.java
+++ trunk/src/main/java/com/leweiyou/shiro/entry/SessionUser.java
@@ -1,7 +1,9 @@
 package com.leweiyou.shiro.entry;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -12,12 +14,21 @@ import java.util.Set;
 public class SessionUser implements Serializable{
 	
 	private static final long serialVersionUID = 8541307043821592596L;
+	
+	//用户ID
 	private String userId = null;
+	//用户登录账号名
 	private String account = null;
+	
+	//用户所属于的角色集合
 	private Set<String> roleIds = new HashSet<String>();
+	
+	//用户拥有的权限
 	private Set<String> rights = new HashSet<String>();
 	private String tree = null;
 	
+	//可扩展属性
+	private Map<String,Object> extAttrs = new HashMap<String, Object>();
 	
 	public String getUserId() {
 		return userId;
@@ -48,6 +59,12 @@ public class SessionUser implements Serializable{
 	}
 	public void addRoleId(String roleId) {
 		this.roleIds.add(roleId);
+	}
+	public Map<String, Object> getExtAttrs() {
+		return extAttrs;
+	}
+	public void setExtAttrs(Map<String, Object> extAttrs) {
+		this.extAttrs = extAttrs;
 	}
 	
 	
