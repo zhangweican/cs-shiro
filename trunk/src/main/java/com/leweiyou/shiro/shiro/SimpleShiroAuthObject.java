6da5b15b9d02f7ab4a955a882adab47a21f6ff20
zhangweican <515632400@qq.com>
2016-06-27 15:07:37 +0800

    修改user对象对外可见

--- trunk/src/main/java/com/leweiyou/shiro/shiro/SimpleShiroAuthObject.java
+++ trunk/src/main/java/com/leweiyou/shiro/shiro/SimpleShiroAuthObject.java
@@ -8,7 +8,7 @@ package com.leweiyou.shiro.shiro;
 public class SimpleShiroAuthObject extends ShiroAuthObject {
 
 	@Override
-	public User getCompareUser() {
+	public ShiroAuthObject.User getCompareUser() {
 		return new User("admin",ShiroEndecryptUtils.md5Password("admin", "123456"));
 	}
 
